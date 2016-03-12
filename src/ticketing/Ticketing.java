0201982e81e9a53068b4636ee12d5c84e5b86ae9
rosekumala <radenroro.rk@gmail.com>
2016-03-12 18:04:56 +0700

    Added files via upload

--- src/ticketing/Ticketing.java
+++ src/ticketing/Ticketing.java
@@ -16,6 +16,26 @@ public class Ticketing {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Gerbong g1 = new Gerbong("Ekonomi",50);
+	Gerbong g2 = new Gerbong("Bisnis",50);
+	Gerbong g3 = new Gerbong("Eksekutif",50);
+	Kereta k1 = new Kereta("123AKU","Sanjaya","Budi");
+	Stasiun s1 = new Stasiun("JKD","JAKARTA PUSAT");
+	Stasiun s2 = new Stasiun("JJJ","JAKAJAKAJAKA");
+	Stasiun s3 = new Stasiun("BDG","BANDUNG");
+	Rute r1 = new Rute(3);
+	Tiket t1 = new Tiket("12 Januari 2016","SANTI",3,"Ekonomi","Jakarta","Bandung");
+	k1.addGerbong(g1);
+	k1.addGerbong(g2);
+	k1.addGerbong(g3);
+	r1.addStasiun(s1);
+	r1.addStasiun(s2);
+	r1.addStasiun(s3);
+	t1.setKereta(k1);
+	t1.setRute(r1);
+	t1.setHargaTiket();
+	t1.setNKursi();
+	t1.cetakTiket();
     }
     
 }
