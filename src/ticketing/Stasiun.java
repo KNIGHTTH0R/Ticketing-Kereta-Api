dcccdbf81261494957cb552012515ea7d7e847c9
nabilbaguspratama <nabilbaguspratama@ymail.com>
2016-03-12 21:39:21 +0700

    Added files via upload
    class Gerbong sama Stasiun

--- src/ticketing/Stasiun.java
+++ src/ticketing/Stasiun.java
@@ -1,36 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tubespbo;
-
-/**
- *
- * @author Dika
- */
-public class Stasiun {
-    private String kodeStasiun;
-    private String namaStasiun;
+public class Stasiun{
+	private String kodeStasiun;
+	private String namaStasiun;
 	
-    public Stasiun(String kodeStasiun, String namaStasiun){
-	this.kodeStasiun=kodeStasiun;
-	this.namaStasiun=namaStasiun;
-    }
-    public void setKodeStasiun(String kodeStasiun){
-    	this.kodeStasiun=kodeStasiun;
-    }
+	public Stasiun(String kodeStasiun, String namaStasiun){
+		this.kodeStasiun = kodeStasiun;
+		this.namaStasiun = namaStasiun;
+	}
 	
-    public void setNamaStasiun(String namaStasiun){
-	this.namaStasiun=namaStasiun;
-    }
+	public void setKodeStasiun(String kodeStasiun){
+		this.kodeStasiun = kodeStasiun;
+	}
+	public void setNamaStasiun(String namaStasiun){
+		this.namaStasiun = namaStasiun;
+	}
+	public String getKodeStasiun(){
+		return kodeStasiun;
+	}
 	
-    public String getKodeStasiun(){
-	return kodeStasiun;
-    }
-	
-    public String getNamaStasiun(){
-	return namaStasiun;
-    }
+	public String getNamaStasiun(){
+		return namaStasiun;
+	}
+	public String toString(){
+		return "Nama Stasiun "+getNamaStasiun();
+	}
 }
-
